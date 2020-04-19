adcc99892229c94e03c6fdf961d4647112ec44f1
Fabien Bellanger <valentil@gmail.com>
2020-04-19 23:08:35 +0200

    Test borrowing

--- test_1/src/main.rs
+++ test_1/src/main.rs
@@ -1,3 +1,24 @@
+fn test(s: &mut String) {
+    s.push_str("_1");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut s = "Test".to_owned();
+    println!("{}", s);
+
+    test(&mut s);
+    println!("{}", s);
+
+    test(&mut s);
+    println!("{}", s);
+
+    // let mut r = "Toto".to_owned();
+    let mut r = 42;
+    let t = &mut r;
+    println!("t: {}", t);
+    *t += 1;
+    let u = &mut r;
+    *u += 5;
+    println!("u: {}", u);
+    println!("r: {}", r);
 }
