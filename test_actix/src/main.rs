a13af991ddad7db1598b4a0760957f44ed6b4434
Fabien Bellanger <valentil@gmail.com>
2020-04-27 23:06:36 +0200

    Route avec JSON

--- test_actix/src/main.rs
+++ test_actix/src/main.rs
@@ -1,9 +1,23 @@
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use actix_web::middleware::Logger;
 use env_logger::Env;
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Status {
+    status: &'static str,
+    message: String,
+}
 
 async fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello world!\n")
+}
+
+async fn status_json() -> Result<web::Json<Status>, ()> {
+    Ok(web::Json(Status {
+        status: "OK",
+        message: "Success".to_owned(),
+    }))
 }
 
 #[actix_rt::main]
@@ -14,6 +28,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .wrap(Logger::new("%t | %s | %r | %a | %Ts"))
             .route("/", web::get().to(index))
+            .route("/json", web::get().to(status_json))
     })
     .bind("127.0.0.1:8088")?
     .run()
