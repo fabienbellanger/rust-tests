671ddb10ec16d04120e3e489d0a395ea4077b82f
Fabien Bellanger <valentil@gmail.com>
2020-05-05 18:33:08 +0200

    Test Diesel en commentaire

--- test_rocket/src/main.rs
+++ test_rocket/src/main.rs
@@ -2,12 +2,29 @@
 
 #[macro_use] extern crate rocket;
 
+// use rocket_contrib::databases::diesel;
+
 mod handler;
 mod fairing;
+mod error;
+
+// #[database("mysql")]
+// struct MyDatabase(diesel::MysqlConnection);
 
 fn main() {
     rocket::ignite()
         .attach(fairing::RequestTimer)
-        .mount("/", routes![handler::index, handler::json, handler::big_json, handler::time_now])
+        // .attach(MyDatabase::fairing())
+        .mount("/", routes![
+            handler::index, 
+            handler::json, 
+            handler::big_json, 
+            handler::time_now,
+        ])
+        .register(
+            catchers![
+                error::not_found,
+                error::internal_server_error],
+        )
         .launch();
 }
