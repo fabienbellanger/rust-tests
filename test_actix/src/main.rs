612afde266c6c560ded4ee1cd815e8b723239a76
Fabien Bellanger <valentil@gmail.com>
2020-05-27 23:11:07 +0200

    Ajout d'une route pour Actix-web

--- test_actix/src/main.rs
+++ test_actix/src/main.rs
@@ -1,5 +1,5 @@
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-//use actix_web::middleware::Logger;
+use actix_web::middleware::Logger;
 use env_logger::Env;
 use serde::Serialize;
 
@@ -20,17 +20,37 @@ async fn status_json() -> Result<web::Json<Status>, ()> {
     }))
 }
 
+#[derive(Serialize)]
+pub struct Task {
+    id: u32,
+    name: &'static str,
+    message: String,
+}
+
+async fn big_json() -> Result<web::Json<Vec<Task>>, ()> {
+    let mut v: Vec<Task> = Vec::new();
+    for i in 0..10_000 {
+        v.push(Task{
+            id: i, 
+            name: "Coucou ceci est mon nom", 
+            message: String::from("Mon message doit être un peu long pour augmenter la taille"),
+        });
+    }
+    Ok(web::Json(v))
+}
+
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     env_logger::from_env(Env::default().default_filter_or("info")).init();
 
     HttpServer::new(|| {
         App::new()
-            //.wrap(Logger::new("%t | %s | %r | %a | %Ts"))
+            .wrap(Logger::new("%t | %s | %r | %a | %Ts"))
             .route("/", web::get().to(index))
             .route("/json", web::get().to(status_json))
-    })
-    .bind("127.0.0.1:8088")?
+            .route("/big-json", web::get().to(big_json))
+        })
+    .bind("localhost:8088")?
     .run()
     .await
 }
